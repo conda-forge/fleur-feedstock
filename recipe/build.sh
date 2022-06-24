ae768ad3f1ef2dc3a75c1e7d79b1265adcb92f7b
Henning Janssen <henning.janssen@gmx.net>
2022-06-24 14:59:03 +0200

    Update build.sh to replace nul characters
    in `compileinfo.h` and `buildinfo.h`

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,7 +14,21 @@ export FLEUR_LIBRARIES="-L${PREFIX}/lib;-lfftw3;-lxml2;-lblas;-llapack;-lscalapa
 #export CFLAGS="${CFLAGS} -I$"
 ./configure.sh
 
-cd build; make; cd -
+cd build
+
+echo "Before"
+grep -Pa '\x00' --color=never include/buildinfo.h | cat -vET
+grep -Pa '\x00' --color=never include/compileinfo.h | cat -vET
+
+sed 's/\x0//g' include/buildinfo.h > include/buildinfo.h
+sed 's/\x0//g' include/compileinfo.h > include/compileinfo.h
+
+echo "After"
+grep -Pa '\x00' --color=never include/buildinfo.h | cat -vET
+grep -Pa '\x00' --color=never include/compileinfo.h | cat -vET
+
+make
+cd -
 
 mkdir -p ${PREFIX}/bin
 cp build/fleur_MPI ${PREFIX}/bin
