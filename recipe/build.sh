cedcbf9aef9d7802363403a9fa1ec8ef844bd1b0
janssenhenning <he.janssen@fz-juelich.de>
2022-08-12 17:00:10 +0200

    Next try

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 set -ex
 
+ls ${PREFIX}/include
+ls ${PREFIX}/include/libxml2
+
 export CC="mpicc"
 export CXX="mpicxx"
-export FC=mpif90 
-export FLEUR_INCLUDEDIR="${PREFIX}/include ${PREFIX}/include/libxml2"
-export FLEUR_LIBRARIES="-lfftw3;-lxml2;-lopenblas;-llapack;-lscalapack;"
+export FC=mpif90
+if [[ ! -z "$MACOSX_DEPLOYMENT_TARGET" ]]; then
+  export FLEUR_INCLUDEDIR="${PREFIX}/include ${PREFIX}/include/libxml2"
+  export FLEUR_LIBRARIES="-lfftw3;-lxml2;-lblas;-llapack;-lscalapack;"
+else
+  export FLEUR_INCLUDEDIR="${PREFIX}/include ${PREFIX}/include/libxml2"
+  export FLEUR_LIBRARIES="-lfftw3;-lxml2;-lopenblas;-llapack;-lscalapack;"
+fi
+
 
 ./configure.sh
 
